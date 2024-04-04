a8092b4fedefefdd5dfa946f5a82eafc55518fa3
KAEL <vincent.achy@gmail.com>
2024-04-04 17:37:57 +0200

    commit

---
+++ front/src/pages/index.js
@@ -0,0 +1,29 @@
+// front/src/pages/index.js
+import Link from "next/link";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Bienvenue sur gBlog</h1>
+      <p>
+        Articles sur différents sujets.
+      </p>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/articles" legacyBehavior>
+              <a>Voir tous les articles</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" legacyBehavior>
+              <a>À propos du blog</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
