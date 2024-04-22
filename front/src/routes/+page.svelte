caad497e699a14c5c7c8b40bd38c33fb88ba9c03
KAEL <vincent.achy@gmail.com>
2024-04-22 18:22:25 +0200

    commit

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <!-- src/routes/+page.svelte -->
+
 <script lang="ts">
+	import type { Article } from '../app.d.ts';
 	import '../styles/home.scss';
 	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import type { Article } from '../app.d.ts'; // Correction du chemin si nécessaire
 
-	let articles: Article[] = []; // Modification ici pour utiliser directement Article
+	let articles: Article[] = [];
 
 	onMount(async () => {
 		const response = await fetch('http://localhost:3000/articles');
@@ -13,20 +13,12 @@
 			articles = await response.json();
 		}
 	});
-
-	function navigateToArticle(id: number) {
-		goto(`/articles/${id}`);
-	}
 </script>
 
 <div class="gallery">
 	{#each articles as article}
-		<button
-			class="article-preview"
-			on:click={() => navigateToArticle(article.id)}
-			aria-label={`Read more about ${article.title}`}
-		>
+		<div class="article-preview">
 			<p class="article-title">{article.title}</p>
-		</button>
+		</div>
 	{/each}
 </div>
