e454c6002a9de932ee4b96fefaf0112e58eeeff2
KAEL <vincent.achy@gmail.com>
2024-05-01 19:12:29 +0200

    commit

--- front/src/lib/Title.svelte
+++ front/src/lib/Title.svelte
@@ -1,20 +1,22 @@
 <!-- src/lib/Title.svelte -->
 <script>
-	export let title = 'gBlog';
+	export let title = 'Blog';
 </script>
 
 <h1>{title}</h1>
 
 <style lang="scss">
 	h1 {
-		color: rgb(238, 238, 238); 
-		background-color: rgb(31, 31, 31); 
-		text-align: center; 
-		width: 100%; 
-		padding: 4px 0; 
-		margin: 0; 
-		text-transform: uppercase;
-		font-weight: 900;
+		color: rgb(238, 238, 238);
+		background-color: rgb(31, 31, 31);
+		text-align: center;
+		width: 100%;
+		padding: 4px 0;
+		margin: 0;
+		// text-transform: uppercase;
+		letter-spacing: -3.2px;
+		font-weight: 100;
 		font-size: 2.5rem;
+		font-family: 'Orbitron', sans-serif;
 	}
 </style>
