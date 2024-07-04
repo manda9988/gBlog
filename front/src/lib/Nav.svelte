3a3bcc8be0b5762cabeb76ea4baa4437074e8c8b
KAEL <vincent.achy@gmail.com>
2024-07-04 23:40:15 +0200

    commit

--- front/src/lib/Nav.svelte
+++ front/src/lib/Nav.svelte
@@ -1,16 +1,11 @@
 <!-- src/lib/Nav.svelte -->
 
 <script lang="ts">
-	// Importation du store pour obtenir des informations sur la page actuelle
 	import { page } from '$app/stores';
-	// Importation de derived pour créer des stores dérivés
 	import { derived } from 'svelte/store';
-	// Importation du store pour vérifier si l'utilisateur est connecté
 	import { isLoggedIn } from './authStore';
-	// Importation des styles pour la navigation
 	import '../styles/nav.scss';
 
-	// Store dérivé pour obtenir le chemin actuel de l'URL
 	const currentPath = derived(page, ($page) => $page.url.pathname);
 </script>
 
